3edf5532583b0ce9b6945deeaae9f6d05f4667a1
Kurt <kurt.lim@acurus.com.au>
2020-11-15 17:01:46 +1100

    feat: clean up old dtos and add mapper profile

---
+++ GameShelf.API/Endpoints/V1/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GameShelf.Domain.Dtos;
+using GameShelf.Domain.Models;
+
+namespace GameShelf.API.Endpoints.V1
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // Games
+            CreateMap<CreateGame, Game>();
+            CreateMap<UpdateGame, Game>();
+            
+            // Users
+            CreateMap<CreateUser, Domain.Models.User>();
+            CreateMap<UpdateUser, Domain.Models.User>();
+        }
+    }
+}
