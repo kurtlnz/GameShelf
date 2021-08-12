e34df98aa74c1eabaa3b3e579d2abb0e7656cfb5
Kurt <klim049@gmail.com>
2021-08-12 17:41:35 +1000

    Add game service test

---
+++ src/Services/Game/Game.API.UnitTests/Services/GameServiceTests.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Game.API.Services;
+using Xunit;
+
+namespace Game.API.UnitTests.Services
+{
+    public class GameServiceTests
+    {
+        private readonly GameService _service;
+        
+        public GameServiceTests()
+        {
+            _service = new GameService();
+        }
+
+        [Fact]
+        public async Task Should_ReturnAllGames_WhenSuccess()
+        {
+            // Arrange
+            var games = new List<string>()
+            {
+                "Terra Mystica",
+                "Barrage",
+                "Caylus 1303",
+            };
+            
+            // Act
+            var result = await _service.GetAllGames();
+
+            // Assert
+            result.Should().BeEquivalentTo(games);
+        }
+    }
+}
